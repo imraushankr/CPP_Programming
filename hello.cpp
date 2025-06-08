a7cceaff1c1232f07c8485bc8fcd8131cafa5792
Raushan Kumar <raushan911721@gmail.com>
2025-06-08 20:31:55 +0530

    Added CPP hello basic program

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>      // Includes the input-output stream library
+using namespace std;     // Allows use of standard namespace (e.g., cout)
+
+int main() {             // Main function: program entry point
+  cout << "Hello World" << endl;  // Outputs "Hello World" followed by a newline
+  return 0;              // Signals successful program termination
+}
